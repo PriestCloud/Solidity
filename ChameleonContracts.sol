55864c3cc9e452b4e61097c4f2a4bc62a6d7da6d
Youngblood Priest <desoyeolayinka@gmail.com>
2022-11-02 22:50:21 +0000

    Add files via upload

---
+++ ChameleonContracts.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identify:MIT'
+
+pragma solidity ^0.5.0;
+
+Contract MakeAConstructor{
+
+
+    string = strrr;
+
+
+
+
+  constructor() public{
+    strrr = "My name is rojag";
+  }
+
+}
